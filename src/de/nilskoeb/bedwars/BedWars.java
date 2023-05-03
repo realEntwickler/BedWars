4860012ca7ef5ca79397e42df4587ae5df56644f
Nils Kevin Körting-Eberhardt <nils@familieeberhardt.de>
2023-05-03 17:36:08 +0200

    added test command for location manager

--- src/de/nilskoeb/bedwars/BedWars.java
+++ src/de/nilskoeb/bedwars/BedWars.java
@@ -1,6 +1,8 @@
 package de.nilskoeb.bedwars;
 
+import de.nilskoeb.bedwars.commands.TestCommand;
 import de.nilskoeb.bedwars.launcher.Launcher;
+import de.nilskoeb.bedwars.manager.LocationManager;
 import de.nilskoeb.bedwars.manager.TeamManager;
 
 public class BedWars {
@@ -9,13 +11,20 @@ public class BedWars {
 
     private final Launcher launcher;
     private final TeamManager teamManager;
+    private final LocationManager locationManager;
 
     public BedWars(Launcher launcher) {
         instance = this;
 
         this.launcher = launcher;
         this.teamManager = new TeamManager();
+        this.locationManager = new LocationManager();
 
+        launcher.getCommand("test").setExecutor(new TestCommand());
+    }
+
+    public LocationManager getLocationManager() {
+        return locationManager;
     }
 
     public TeamManager getTeamManager() {
