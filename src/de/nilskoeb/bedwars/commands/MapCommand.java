f598b1409a87cbca79b00580d2d80722f1be88dd
Nils Kevin Körting-Eberhardt <nils@familieeberhardt.de>
2023-05-03 18:16:29 +0200

    added tp in MapCommand

--- src/de/nilskoeb/bedwars/commands/MapCommand.java
+++ src/de/nilskoeb/bedwars/commands/MapCommand.java
@@ -1,9 +1,13 @@
 package de.nilskoeb.bedwars.commands;
 
 import de.nilskoeb.bedwars.utilities.$;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class MapCommand implements CommandExecutor {
 
@@ -14,8 +18,22 @@ public class MapCommand implements CommandExecutor {
             commandSender.sendMessage("§8» §c/map add <Name> <Material> <Data>");
             commandSender.sendMessage("§8» §c/map tp <Name>");
             commandSender.sendMessage("§8» §c/map remove <Name>");
-        } else if (strings.length == 1) {
+        } else if (strings.length == 2) {
+            switch (strings[0]) {
+                case "tp":
+                    if (!(commandSender instanceof Player)) {
+                        commandSender.sendMessage($.PREFIX + "Dieses Kommando kann nur als §eSpieler §7ausgeführt werden.");
+                        break;
+                    }
 
+                    World world = Bukkit.createWorld(WorldCreator.name(strings[1]));
+                    ((Player) commandSender).teleport(world.getSpawnLocation());
+                    commandSender.sendMessage($.PREFIX + "Du befindest dich nun auf der Welt §e" + world.getName() + "§7.");
+                    break;
+                default:
+                    commandSender.sendMessage($.PREFIX + "Bitte beachte die §eBenutzung §7dieses Kommandos.");
+                    break;
+            }
         }
         return false;
     }
